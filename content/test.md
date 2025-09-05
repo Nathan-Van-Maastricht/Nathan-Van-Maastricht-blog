21c45edfaa691421ff87f86e6366f4c3d6bbfe32
Nathan Van Maastricht <nathan.vanmaastricht@gmail.com>
2025-09-05 22:00:53 +0930

    Chore: workflow test

---
+++ content/test.md
@@ -0,0 +1,6 @@
+Title: This is a test post
+Date: 2025-09-05 21:32
+Category: Testing
+Tags: testing, workflow
+
+This is a test
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Nathan Van Maastricht'
+SITENAME = 'Nathan Van Maastricht Blog'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Australia/Adelaide'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("LinkedIn", "https://www.linkedin.com/in/nathan-van-maastricht-72a177324/"),
+    ("Github", "https://github.com/Nathan-Van-Maastricht")
+)
+
+# Social widget
+SOCIAL = (
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
